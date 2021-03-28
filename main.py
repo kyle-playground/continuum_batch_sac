deef4fe7a17f36f54228b4e040613b02e30bc4e6
kyle-playground <yungkailiu@gmail.com>
2021-03-28 15:46:03 +0800

    Success send actor to envs and bring exps back

--- main.py
+++ main.py
@@ -7,80 +7,62 @@ from torch.utils.tensorboard import SummaryWriter
 import datetime
 from arguments import argparser
 from memory import ReplayMemory
-from multiprocessing import Value, Pipe, Queue, Process
-import queue
+from multiprocessing import Value, Queue, Process
+
 
 args = argparser()
 
 
-def bring_episode_exp(param_queue, conn, total_numsteps, total_episode, num):
-    # set seeds for children processes
-    process_seed = args.seed + num*args.n_actors
-    torch.manual_seed(process_seed)
-    np.random.seed(process_seed)
+def bring_episode_exp(agent, rand_exploring, process_seed, total_numsteps, total_episode, trans_queue):
 
     # create a agent for each process
     env = ContinuumRobotEnv(seed=process_seed, random_obstacle=args.rand_obs)
-    state_dim = env.observation_space()
+    episode_steps = 0
+    done = False
+    state = env.reset()
+    transitions_batch = []
 
-    agent = SAC(state_dim, env, args)
-    exploring_step = 0
+    while not done:
 
-    while True:
-        episode_steps = 0
-        done = False
-        state = env.reset()
+        if rand_exploring:
+            action = env.random_move()  # Sample random action
+        else:
+            action = agent.select_action(state)   # Sample action from policy
 
-        while not done:
+        next_state, reward, done, info = env.step(action)  # Step
 
-            if args.start_steps > exploring_step:
-                action = env.random_move()  # Sample random action
-                exploring_step += 1
-            else:
-                action = agent.select_action(state)   # Sample action from policy
+        # Ignore the "done" signal if it comes from hitting the time horizon.
+        # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
+        mask = 1 if episode_steps == env.MAX_STEP else float(not done)
+        transitions = [state, action, reward, next_state, mask]
 
-            next_state, reward, done, info = env.step(action)  # Step
+        transitions_batch.append(transitions)
+        episode_steps += 1
+        state = next_state
 
-            # Ignore the "done" signal if it comes from hitting the time horizon.
-            # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
-            mask = 1 if episode_steps == env.MAX_STEP else float(not done)
-            transitions = [state, action, reward, next_state, mask]
+        if done:
+            trans_queue.put(transitions_batch)
+            with total_episode.get_lock():
+                total_episode.value += 1
 
-            conn.send(transitions)  # Append transition to memory
-            episode_steps += 1
-            state = next_state
+            with total_numsteps.get_lock():
+                total_numsteps.value += episode_steps
 
-            if done:
-
-                with total_episode.get_lock():
-                    total_episode.value += 1
-                with total_numsteps.get_lock():
-                    total_numsteps.value += episode_steps
-
-                try:
-                    param = param_queue.get(block=False)
-                    agent.policy.load_state_dict(param)
-                except queue.Empty:
-                    pass
-
-        if total_numsteps.value > args.num_steps:
-            break
 
 
 def run():
     # writer
-    writer = SummaryWriter('runs/{}_SAC_{}workers_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
-                                                             args.n_actors, "rand" if args.rand_obs else "uniform"))
+    writer = SummaryWriter('runs/{}_SAC_{}envs'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
+                                                       "1" if not args.rand_obs else args.n_envs))
     # set seeds
     torch.manual_seed(args.seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(args.seed)
     np.random.seed(args.seed)
-
-    # Environment
-    env = ContinuumRobotEnv()
+    process_seeds = [(i+1)*16 for i in range(args.n_envs)]
 
     # Agent
+    env = ContinuumRobotEnv()
     state_dim = env.observation_space()
     if torch.cuda.is_available():
         agent = SAC(state_dim, env, args, gpu=True)
@@ -90,51 +72,52 @@ def run():
 
     # Memory
     memory = ReplayMemory(args.replay_size, args.seed)
-    receiver, sender = Pipe()
-    param_queue = Queue(maxsize=args.n_actors+1)
 
     # Other variables for training process
     total_numsteps = Value('i', 0)
     total_episode = Value('i', 0)
-    process = []
     test_num = 0
     updates = 0
-
-    # get memory from multi env
-    for num in range(args.n_actors):
-        procs = Process(target=bring_episode_exp, args=(param_queue, sender,
-                                                        total_numsteps, total_episode, num))
-        procs.start()
-        process.append(procs)
+    rand_exploring = True
 
     while True:
+        # get memory from multi env
+        if total_numsteps.value > args.start_steps:
+            rand_exploring = True
 
-        for _ in range(args.n_actors):
-            transitions = receiver.recv()
-            memory.push(transitions)
+        # Arguments for child processes
+        processes = []
+        trans_queue = Queue()
+        agent_cpu = agent
+        agent_cpu.mv2cpu()
+        # Send agent to Batch Environments and get batch exps
+        for n in range(args.n_envs):
+            p = Process(target=bring_episode_exp, args=(agent_cpu, rand_exploring, process_seeds[n],
+                                                            total_numsteps, total_episode, trans_queue))
+            p.start()
+            processes.append(p)
+        for process in processes:
+            process.join(timeout=0.01)
+
+            transitions_batch = trans_queue.get()
+            memory.push_batch(transitions_batch)
+        del processes
+        del agent_cpu
 
         if len(memory) > args.batch_size:
-            for _ in range(args.updates_per_step):
+            breakpoint()
+            for _ in range(args.updates_per_collection):
 
                 # Update parameters of all the networks
                 critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory,
                                                                                                      args.batch_size,
                                                                                                      updates)
                 updates += 1
-            if param_queue.full():
-                drop = param_queue.get()
-            state_dict = agent.policy.state_dict()
-            for k, v in state_dict.items():
-                state_dict[k] = v.cpu()
-            param_queue.put(state_dict)
-            del state_dict
 
         if total_numsteps.value > args.num_steps:
-            for p in process:
-                p.terminate()
             break
 
-        if total_episode.value - 10000*(test_num+1) > 0 and args.eval is True:
+        if total_episode.value - 100*(test_num+1) > 0 and args.eval is True:
             test_num += 1
             avg_reward = 0.
             episodes = 10
@@ -145,13 +128,14 @@ def run():
                 episode_reward = 0.
                 done = False
                 state = env.reset()
+                info = "Go~~~"
                 while not done:
                     action = agent.select_action(state, evaluate=True)
-
                     next_state, reward, done, info = env.step(action)
                     episode_reward += reward
                     state = next_state
                 avg_reward += episode_reward
+
                 if info == "is success":
                     num_success += 1
 
