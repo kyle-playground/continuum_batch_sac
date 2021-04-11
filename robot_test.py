b2d6bb21493c2170d6663b8e9614d60a2c568a68
kyle-playground <yungkailiu@gmail.com>
2021-04-11 17:21:22 +0800

    Not yet done haha

--- robot_test.py
+++ robot_test.py
@@ -1,10 +1,2 @@
-from continuum_robot_env import ContinuumRobotEnv
-import time
-env = ContinuumRobotEnv(use_GUI=True, random_obstacle=True, seed=15)
-env.reset()
-for _ in range(10000):
-    env.step(env.random_move())
-    env.visualize_goal(env.goal)
-    time.sleep(1/200.)
 
 
